5f79bbaf9afee46ab17ed191ec54e4e2b338fbff
diganta15 <ddas461598@gmail.com>
2021-09-11 00:55:19 +0530

    Added csv file and found habitable planets

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,40 @@
+const parse = require('csv-parse');
+const fs = require('fs');
+
 const planets = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    //Returns true if it is habitable and gets right amount of light and is of adequate size
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36
+        && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+
+}
+
+fs.createReadStream('../server/data/kepler_data.csv')//Source
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    })/*Destination*/) //Pipe - Connecting readable stream source data to writable destination data. Sends parsed data
+    .on('data', (data) => {
+        if (isHabitablePlanet(data)) { //Checks if the planet is habitable
+            habitablePlanets.push(data); //Pushes data to results variable
+        }
+        planets.push(data);
+
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log(habitablePlanets.map((planet) => {
+            return planet['kepler_name']
+        }))
+        console.log(`${habitablePlanets.length} habitable planets found`);
+        console.log('Done');
+    })
+
 
 module.exports = planets;
