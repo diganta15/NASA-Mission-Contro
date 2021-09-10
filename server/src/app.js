f13bbb24a3a8de458b33e1d021a79c685eb8ad41
diganta15 <ddas461598@gmail.com>
2021-09-11 00:32:08 +0530

    Started working on planets endpoint

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const { planetsRouter} = require('./routes/planets/planets')
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send('Working')
+})
+
+app.use(cors({
+    origin:'http://localhost:3000'
+}));
+app.use(express.json());
+app.use(planetsRouter);
+
+module.exports = app;
+   
